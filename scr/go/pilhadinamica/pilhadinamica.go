0b256a7c6fda41dbe3afedb48533d3b512f25aa2
GabrielAgui373 <aguairg9@gmail.com>
2022-04-30 15:23:31 -0300

    added 'pilha-dinamica' in Go

---
+++ scr/go/pilhadinamica/pilhadinamica.go
@@ -0,0 +1,27 @@
+package pilhadinamica
+
+// import fmt
+
+type List struct {
+	V    float64
+	Next *List
+}
+
+type Stack struct {
+	Top *List
+}
+
+func StackCreate() *Stack {
+	stack := new(Stack)
+	stack.Top = nil
+
+	return stack
+}
+
+func StackPush(s *Stack, v float64) {
+	list := new(List)
+	list.V = v
+	list.Next = s.Top
+
+	s.Top = list
+}
